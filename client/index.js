c8a84a1f9d7f52b2325e6b58e7be2402e32a7612
Tanner Lyon <tannerhlyon@gmail.com>
2022-12-27 18:05:34 -0500

    struggling to get application properly loaded to webpage. Currently trouble shooting webpack. Other issues consiste of: 1) having to perform fetch logic on backend else hitting 401 error 2) cannot access styling/css 3)iteration through data

--- client/index.js
+++ client/index.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 import { BrowserRouter}  from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import App from './App'; 
-import 'styling/styles.css';
+import './styling/styles.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
